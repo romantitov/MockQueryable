4d63117d6f1ffadaaa30a453ce351c8f4c35c16a
Steve Pynylo <stevepy@zoho.com>
2024-09-01 11:51:33 +1000

    Altered namespace for extension method to revert a breaking change with previous extension methods that were consolidated here.

--- src/MockQueryable/MockQueryable.EntityFrameworkCore/MockQueryableExtensions.cs
+++ src/MockQueryable/MockQueryable.EntityFrameworkCore/MockQueryableExtensions.cs
@@ -1,14 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using MockQueryable.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 
-
-namespace MockQueryable.EntityFrameworkCore
+// Moving MockQueryableExtensions BuildMock into the MockQueryable.EntityFrameworkCore
+// namespace had breaking changes with earlier extensions added to MockQueryable.Moq
+// and other previous extension method locations. Moving this extension up a namespace to
+// MockQueryable aleviates that breaking change. It still needs to remain in EF core since it
+// is dependent on the EF Core AsyncEnumerable.
+namespace MockQueryable
 {
     public static class MockQueryableExtensions
     {
         public static IQueryable<TEntity> BuildMock<TEntity>(this IEnumerable<TEntity> data) where TEntity : class
-		{
+        {
             return new TestAsyncEnumerableEfCore<TEntity>(data);
-		}
+        }
     }
 }
